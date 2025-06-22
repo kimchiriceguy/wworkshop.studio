99bd6b55e63a41da0aa5f4f74e31abc658c173ee
kimchiriceguy <ricekimchirice1@gmail.com>
2025-06-23 00:40:36 +0800

    test2calendar.sql

--- test2calendar.sql
+++ test2calendar.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 22, 2025 at 08:55 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `test2calendar`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin_users`
---
 
 CREATE TABLE `admin_users` (
   `id` int(11) NOT NULL,
@@ -33,18 +9,8 @@ CREATE TABLE `admin_users` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `admin_users`
---
-
 INSERT INTO `admin_users` (`id`, `username`, `password`) VALUES
 (1, 'admin', '$2y$10$6CCj1MoZNDyIela/D/zxC.NjYyefDPQjt4Vnt3h8o8HbIVPRVtrpi');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `appointments`
---
 
 CREATE TABLE `appointments` (
   `id` int(11) NOT NULL,
@@ -52,21 +18,11 @@ CREATE TABLE `appointments` (
   `service` varchar(50) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `appointments`
---
+)
 
 INSERT INTO `appointments` (`id`, `barber`, `service`, `date`, `time`) VALUES
 (10, 'Gylliane', 'Shave', '2025-05-03', '14:51:00'),
 (11, 'Dorothy', '', '2025-05-17', '14:51:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `purchases`
---
 
 CREATE TABLE `purchases` (
   `id` int(11) NOT NULL,
@@ -75,61 +31,27 @@ CREATE TABLE `purchases` (
   `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `purchases`
---
-
 INSERT INTO `purchases` (`id`, `price`, `item`, `quantity`) VALUES
 (6, 380.00, 'DUST VOLUME POWER', 2),
 (7, 420.00, 'SEA SALT TEXTURE SPRAY', 1),
 (8, 480.00, 'ORIGINAL WATER-BASED POMADE - REVOLT', 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin_users`
---
 ALTER TABLE `admin_users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
---
--- Indexes for table `appointments`
---
 ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `purchases`
---
 ALTER TABLE `purchases`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin_users`
---
 ALTER TABLE `admin_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `appointments`
---
 ALTER TABLE `appointments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
---
--- AUTO_INCREMENT for table `purchases`
---
 ALTER TABLE `purchases`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
