c36d848c11d2d00a1c263bacee07604c0d6c0af5
kimchiriceguy <ricekimchirice1@gmail.com>
2025-07-01 11:20:30 +0800

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -6,7 +6,7 @@ $username = "root";
 $password = "";
 $database = "test2calendar";
 
-// Connect to DB
+//to test2calendar PLEASE get around to changing the db name
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -16,7 +16,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $inputUsername = $_POST['username'];
     $inputPassword = $_POST['password'];
 
-    // Prepare statement
     $stmt = $conn->prepare("SELECT id, username, password FROM admin_users WHERE username = ?");
     $stmt->bind_param("s", $inputUsername);
     $stmt->execute();
@@ -26,17 +25,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt->bind_result($id, $username, $hashedPassword);
         $stmt->fetch();
 
-        // Verify password
         if (password_verify($inputPassword, $hashedPassword)) {
-            // Correct login
             $_SESSION['admin_logged_in'] = true;
             $_SESSION['admin_username'] = $username;
             header("Location: admin_dashboard.php");
             exit();
         }
     }
-
-    // If we get here, login failed
     header("Location: admin_login.html?error=1");
     exit();
 }
