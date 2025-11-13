98598288fc822504bfc61abe1b24600387047124
kimchiriceguy <ricekimchirice1@gmail.com>
2025-11-13 10:47:30 +0800

    react transfer

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import HomePage from './pages/Home';
+
+function App() {
+  <main className="main-content">
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </main>
+}
+
+export default App;
